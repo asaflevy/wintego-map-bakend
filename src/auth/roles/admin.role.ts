0dd925ab574daed3d5f7269ff83e9874fb85766b
asafart <asaf@artlist.io>
2019-06-12 23:42:48 +0300

    addd

--- src/auth/roles/admin.role.ts
+++ src/auth/roles/admin.role.ts
@@ -1,22 +1,17 @@
-
-import {
-    ExecutionContext,
-    Injectable,
-    UnauthorizedException,
-    HttpStatus,
-    HttpException,
-} from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-import { UsersService } from 'src/users/users.service';
+import {ExecutionContext, HttpException, HttpStatus, Injectable,} from '@nestjs/common';
+import {AuthGuard} from '@nestjs/passport';
+import {UsersService} from '../../users/users.service';
 
 @Injectable()
 export class AdminRoleGuard extends AuthGuard('jwt') {
 
     body = '';
     params: any = {};
+
     constructor(private userSrv: UsersService) {
         super();
     }
+
     canActivate(context: ExecutionContext) {
         const request = context.switchToHttp().getRequest();
         this.body = request.body;
@@ -25,23 +20,18 @@ export class AdminRoleGuard extends AuthGuard('jwt') {
     }
 
     handleRequest(err, user, info) {
-        console.log("user",user)
-        if (err || !user || user.role!==1) {
+
+        if (err || !user || user.role !== 1) {
             throw err || new HttpException({}, HttpStatus.UNAUTHORIZED);
-            // return new HttpException({}, HttpStatus.FORBIDDEN);
-            // return null;
         }
 
-        if (!this.params) return;
+        if (!this.params) {
+            return;
+        }
         const id = this.params.id;
-        if (!id) return new HttpException({}, HttpStatus.FORBIDDEN);
-        // const promise: Promise<any> = this.userSrv.findById(id)
-        // promise.then(user => {
-        //     return new HttpException({}, HttpStatus.FORBIDDEN);
-        // }).catch(e => {
-        //     // throw new HttpException({ e }, HttpStatus.FORBIDDEN);
-        //     return e;
-        // });
+        if (!id) {
+            return new HttpException({}, HttpStatus.FORBIDDEN);
+        }
         return user;
     }
 }
